8da9db2ad3f926cc6cf3fa135ded6d360301fcb8
shuparb <parabshubham8@gmail.com>
2021-10-22 16:36:09 +0530

    Added analyze

--- timeshit.py
+++ timeshit.py
@@ -1,9 +1,10 @@
 import argparse
-from datetime import datetime
+from datetime import *
 import os
 import json
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
+import re
 
 date = ""
 
@@ -66,7 +67,7 @@ def remove(timestamps):
 
 # view whole day info in table/pie format
 def view():
-    print("Viewing date's activity")
+    print("Viewing date: "+date+"'s activity")
     data = {}
     with open("./files/"+date+".json", "r+") as f:
         data = json.load(f)
@@ -77,7 +78,7 @@ def view():
     info = {}
     for each in data.keys():
         if pivot[1] != data[each] and each != "0000":
-            list(info.values())[len(info.values())-1].setdefault("hours",round(hr/60, 2))
+            list(info.values())[len(info.values())-1].setdefault("hours",round(hr/60, 1))
             list(info.values())[len(info.values())-1].setdefault("toHr", pivot[0])
             hr = 0
             display = True
@@ -89,14 +90,23 @@ def view():
                 info[each] = {"workType":"Unallocated","note":""}
             display = False
         pivot = (each,data[each])
-    list(info.values())[len(info.values())-1].setdefault("hours",round(hr/60, 2))
+    list(info.values())[len(info.values())-1].setdefault("hours",round(hr/60, 1))
     list(info.values())[len(info.values())-1].setdefault("toHr", "2400")
     #
     print("-"*60)
     table = PrettyTable(["From", "To", "Work Type", "Hours Spend", "Note"])
     work_type = []
     hours_spend = []
+    util = 0
+    slack = 0
+    unalloc = 0
     for each in info:
+        if info[each]["workType"] in ["reading", "coding", "maths"]:
+            util = util + info[each]["hours"]
+        if info[each]["workType"] in ["surfing", "watching", "drawing"]:
+            slack= slack + info[each]["hours"]
+        if info[each]["workType"] == "Unallocated":
+            unalloc = unalloc + info[each]["hours"]
         table.add_row([each, info[each]["toHr"], info[each]["workType"],info[each]["hours"],info[each]["note"]])
         work_type.append(info[each]["workType"])
         hours_spend.append(info[each]["hours"])
@@ -105,30 +115,100 @@ def view():
     fig1, ax1 = plt.subplots()
     ax1.pie(hours_spend, labels = work_type, autopct='%1.1f%%')
     ax1.title.set_text("Date: "+date)
+    print("Productive Hours: "+str(util)+" ("+str(round(util/24, 3)*100)+" %)")
+    print("Slacking Hours: "+str(slack)+" ("+str(round(slack/24, 3)*100)+" %)")
+    print("Unallocated Hours: "+str(unalloc)+" ("+str(round(unalloc/24, 3)*100)+" %)")
     plt.show()
 
+def analyze(worktype, days):
+    start = datetime.today() - timedelta(days=days)
+    if len(os.listdir("./files")) > 0:
+        avail_start = datetime.strptime(os.listdir("./files").pop(0).split(".")[0],"%y-%m-%d")
+        delta = start - avail_start
+        if delta.days > 0 :
+            avail_start = start
+        print("Starting analysis from {}".format(avail_start.strftime("%y-%m-%d")))
+        all = []
+        while avail_start < datetime.today() - timedelta(days=1):
+            data = {}
+            try:
+                with open("./files/"+avail_start.strftime("%y-%m-%d")+".json", "r+") as f:
+                    data = json.load(f)
+            except FileNotFoundError:
+                print("{} file not found".format(avail_start.strftime("%y-%m-%d")+".json"))
+                avail_start += timedelta(days=1)
+                continue
+            #
+            mins = 0
+            for i in data.keys():
+                each = data[i]
+                if each != None:
+                    if each["workType"] in worktype:
+                        mins += 1
+            all.append(mins/60)
+            avail_start += timedelta(days=1)
+        print("Calculated {} data for {} days. Total hours: {:.3f} hours ({:.3f} %)".format(worktype, len(all), sum(all), (sum(all)/(len(all)*24))*100))
+        print("min {:.3f} hrs max {:.3f} hrs avg {:.3f} hrs".format(min(all), max(all), sum(all)/len(all)))
+    else:
+        print("no log files")
+
 if __name__ == '__main__':
-    CHOICES = ["add", "remove", "view"]
+    CHOICES = ["add", "remove", "view", "analyze"]
     WORKTYPE = ["sleeping", "office work", "reading book", "coding", "custom"]
     parser = argparse.ArgumentParser(description="Welcome to shuparb's timesheet!")
-    parser.add_argument("-A", "--action", choices=CHOICES,help="Action to perform", required=True)
-    parser.add_argument("--timestamp", nargs=2, help="Start & End timestamp of activity (format=HHMM)")
-    parser.add_argument("--worktype", choices=WORKTYPE, help="Type of work you've done")
-    parser.add_argument("--note", help="Any remark")
-    parser.add_argument("--date", help="Specify date (format=YY-MM-DD)")
+    parser.add_argument("-a", "--action", choices=CHOICES,help="Action to perform", required=True)
+    parser.add_argument("-t", "--timestamp", nargs=2, help="Start & End timestamp of activity (24hrs format=HHMM)")
+    parser.add_argument("-w", "--worktype", choices=WORKTYPE, help="Type of work you've done")
+    parser.add_argument("-n", "--note", help="Any remark")
+    parser.add_argument("-d", "--date", help="Specify date (format=YY-MM-DD)")
+    parser.add_argument("--days", help="Number of days to analyze")
     args = parser.parse_args()
+    datepat = re.compile(r'[\d]{2}-[\d]{2}-[\d]{2}')
+    if args.date != None and re.fullmatch(datepat, args.date) == None:
+        print("ERR: Wrong date pattern: {}".format(args.date))
+        exit(1)
     date = datetime.today().strftime("%y-%m-%d") if args.date == None else args.date
     # creating required directories & files
     if "files" not in os.listdir():
         os.mkdir("files")
-    if date+".json" not in os.listdir("./files"):
+    if date+".json" not in os.listdir("./files") and args.action != "view":
         with open("./files/"+date+".json", "w") as f: 
-            print("Creating date")
+            print("Creating date {}".format(date))
             json.dump(createDay(), f)
-    #
+    elif date+".json" not in os.listdir("./files") and args.action == "view":
+        print("Invalid date")
+        exit(1)
     if args.action == "add":
+        if args.worktype == None:
+            print("ERR: Pls provide worktype to add")
+            exit(1)
+        if args.timestamp == None and len(args.timestamp) == 2:
+            print("ERR: timestamp missing")
+            exit(1)
+        timepat = re.compile(r'[0-2][0-9][0-5][0-9] [0-2][0-9][0-5][0-9]')
+        if re.fullmatch(timepat, args.timestamp[0]+" "+args.timestamp[1]) == None:
+            print("ERR: timestamp invalid Pattern (format=HHMM HHMM)")
+            exit(1)
         add(args.timestamp, workType=args.worktype, note=args.note)
     elif args.action == "remove":
         remove(args.timestamp)
     elif args.action == "view":
         view()
+    elif args.action == "analyze":
+        if args.days == None:
+            args.days = "30"
+        if args.worktype == None:
+            choose = int(input("Choose work type: 1)Utilize 2)Slack 3)Office 4)Sleeping"))
+            if choose == 1:
+                analyze(["reading", "coding", "maths"], int(args.days))
+                exit(0)
+            if choose == 2:
+                analyze(["surfing", "watching", "drawing"], int(args.days))
+                exit(0)
+            if choose == 3:
+                analyze(["office"], int(args.days))
+                exit(0)
+            if choose == 4:
+                analyze(["sleeping"], int(args.days))
+                exit(0)
+        analyze([args.worktype], int(args.days))
